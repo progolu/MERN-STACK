f1cfc4f1bce2c178ad2842770f3e7d783e9534d0
akanniolusegun5@gmail.com <akanniolusegun5@gmail.com>
2023-07-09 16:30:44 +0100

    url

--- backend/routes/blog.js
+++ backend/routes/blog.js
@@ -10,18 +10,18 @@ const {
 const router = express.Router()
 
 //GET all blog
-router.get('/', getBlogs)
+router.get('https://mern-stack-steel.vercel.app/', getBlogs)
 
 //GET single blog
-router.get('/:id', getBlog)
+router.get('https://mern-stack-steel.vercel.app/:id', getBlog)
 
 //POST new blog
-router.post('/', createBlog)
+router.post('https://mern-stack-steel.vercel.app/', createBlog)
 
 //UPDATE new blog
-router.patch('/:id', updateBlog)
+router.patch('https://mern-stack-steel.vercel.app/:id', updateBlog)
 
 //DELETE blog
-router.delete('/:id', deleteBlog)
+router.delete('https://mern-stack-steel.vercel.app/:id', deleteBlog)
 
 module.exports = router
