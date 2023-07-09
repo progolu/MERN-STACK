bb71eb0f049770d229dbe384ea1efb40a7a44d80
akanniolusegun5@gmail.com <akanniolusegun5@gmail.com>
2023-07-09 15:42:33 +0100

    first push

---
+++ backend/server.js
@@ -0,0 +1,41 @@
+require('dotenv').config()
+
+const express = require('express')
+const mongoose = require('mongoose')
+const workoutRoutes = require('./routes/workouts')
+const blogRoutes = require('./routes/blog')
+
+
+
+//express app
+const app = express()
+
+//middleware
+app.use(express.json())
+
+app.use((req, res, next)=>{
+    console.log(req.path, req.method)
+    next()
+})
+
+//routes
+// app.get('/', (req, res) => {
+//     res.json({msg: 'Welcome to the application'})
+// })
+app.use('/api/workouts', workoutRoutes)
+app.use('/api/blog', blogRoutes)
+
+
+//connect to db
+mongoose.connect(process.env.MONGO_URI)
+.then(() =>{
+    
+//listen for request
+app.listen(process.env.PORT, ()=>{
+   // console.log('listening on port 4000v', process.env.PORT)
+    console.log('listening on port 4000')
+})
+})
+.catch((error)=>{
+    console.log(error)
+})
