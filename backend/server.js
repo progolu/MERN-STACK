42ff1185b01072c692b3dc73061a68711cb6ae8f
akanniolusegun5@gmail.com <akanniolusegun5@gmail.com>
2023-07-09 22:28:01 +0100

    link react backend to vercel

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,10 @@ const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
 const blogRoutes = require('./routes/blog')
 
+const path = require("path")
+//const logger = require("morgan")
+const cors = require("cors")
+
 
 
 //express app
@@ -12,8 +16,11 @@ const app = express()
 
 //middleware
 app.use(express.json())
-
-app.use((req, res, next)=>{
+// app.use(logger("dev"))
+// app.use(express.json())
+// app.use(express.urlencoded({ extended: false }))
+// app.use(cors())
+app.use((req, res, next) => {
     console.log(req.path, req.method)
     next()
 })
@@ -26,16 +33,29 @@ app.use('/api/workouts', workoutRoutes)
 app.use('/api/blog', blogRoutes)
 
 
+app.use(express.static(path.join(__dirname, "./frontend/build")))
+
+app.get("*", function (_, res) {
+    res.sendFile(
+        path.join(__dirname, "./frontend/build/index.html"),
+        function (err) {
+            if (err) {
+                res.status(500).send(err)
+            }
+        }
+    )
+})
+
 //connect to db
 mongoose.connect(process.env.MONGO_URI)
-.then(() =>{
-    
-//listen for request
-app.listen(process.env.PORT, ()=>{
-   // console.log('listening on port 4000v', process.env.PORT)
-    console.log('listening on port 4000')
-})
-})
-.catch((error)=>{
-    console.log(error)
-})
+    .then(() => {
+
+        //listen for request
+        app.listen(process.env.PORT, () => {
+            // console.log('listening on port 4000v', process.env.PORT)
+            console.log('listening on port 4000')
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
